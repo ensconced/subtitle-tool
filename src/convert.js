4c0fdf0fbf34600a9e9bd067b6d1ed7cfe71aee6
Joe Barnett <joebarnett876@gmail.com>
2020-10-26 20:28:19 +0000

    refactor and use lodash

--- src/convert.js
+++ src/convert.js
@@ -1,4 +1,9 @@
+const chunk = require("lodash/chunk");
+
 const MAX_SECTION_LENGTH = 15 * 1000;
+const MS_PER_SEC = 1000;
+const MS_PER_MINUTE = 60 * MS_PER_SEC;
+const MS_PER_HOUR = 60 * MS_PER_MINUTE;
 
 function time(str) {
   const [hms, ms] = str.split(",");
@@ -10,37 +15,28 @@ function parseTimeRange(rangeStr) {
   return rangeStr.split("-->").map((x) => time(x.trim()));
 }
 
+function sectionIsFull(section) {
+  return section.end - section.start >= MAX_SECTION_LENGTH;
+}
+
 function getSections(originalString) {
   const lines = originalString.split("\n");
-  const sections = [];
-  let currentSection = { text: "" };
-  lines.forEach((line, lineIdx) => {
-    switch (lineIdx % 4) {
-      case 1:
-        const [lineStart, lineEnd] = parseTimeRange(line);
-        currentSection.end = lineEnd;
-        // if it's a new section, need to set the start
-        if (!("start" in currentSection)) {
-          currentSection.start = lineStart;
-        }
-        // always need to update the end
-        currentSection.end = lineEnd;
-        break;
-      case 2:
-        currentSection.text += ` ${line.trim()}`;
-        break;
-      default:
-        // nothing
-        break;
+  const sections = chunk(lines, 4);
+  return sections.reduce((result, section) => {
+    const [, range, text] = section;
+    if (!range) return result;
+    const [lineStart, lineEnd] = parseTimeRange(range);
+    if (result.length === 0 || sectionIsFull(result[result.length - 1])) {
+      // add new section
+      result.push({ start: lineStart, end: lineEnd, text: text.trim() });
+    } else {
+      // add to current section
+      const currentSection = result[result.length - 1];
+      currentSection.end = lineEnd;
+      currentSection.text += ` ${text.trim()}`;
     }
-    const sectionIsFull =
-      currentSection.end - currentSection.start >= MAX_SECTION_LENGTH;
-    if (sectionIsFull || lineIdx === lines.length - 1) {
-      sections.push(currentSection);
-      currentSection = { text: "" };
-    }
-  });
-  return sections.map((section) => ({ ...section, text: section.text.trim() }));
+    return result;
+  }, []);
 }
 
 function leftPad(num) {
@@ -49,11 +45,11 @@ function leftPad(num) {
 }
 
 function timeStamp(ms) {
-  const hours = Math.floor(ms / (60 * 60 * 1000));
-  let rem = ms - hours * (60 * 60 * 1000);
-  const mins = Math.floor(rem / (60 * 1000));
-  rem = rem - mins * (60 * 1000);
-  const secs = Math.floor(rem / 1000);
+  const hours = Math.floor(ms / MS_PER_HOUR);
+  let rem = ms - hours * MS_PER_HOUR;
+  const mins = Math.floor(rem / MS_PER_MINUTE);
+  rem = rem - mins * MS_PER_MINUTE;
+  const secs = Math.floor(rem / MS_PER_SEC);
   return [hours, mins, secs].map(leftPad).join(":");
 }
 
